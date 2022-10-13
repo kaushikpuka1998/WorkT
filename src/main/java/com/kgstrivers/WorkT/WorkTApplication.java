c6a2b2d38e2cf2fb8422f8ff3ed6f4acf5f6f748
kaushik.ghosh <kaushik.ghosh@intrcity.com>
2022-10-13 21:21:57 +0530

    Adding Initial Codebase

---
+++ src/main/java/com/kgstrivers/WorkT/WorkTApplication.java
@@ -0,0 +1,15 @@
+package com.kgstrivers.WorkT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkTApplication {
+
+	public static void main(String[] args) {
+
+		System.out.println("HEllo World");
+		SpringApplication.run(WorkTApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kgstrivers/WorkT/WorkTApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kgstrivers.WorkT;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkTApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
