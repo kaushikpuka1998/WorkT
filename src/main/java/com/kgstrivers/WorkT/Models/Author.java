a0ee2a27010825eb00f20f5f5441ecd3e5b28da7
kaushik.ghosh <kaushik.ghosh@intrcity.com>
2022-10-13 22:41:47 +0530

    Adding to_string and hashcode and equals function in author entity

--- src/main/java/com/kgstrivers/WorkT/Models/Author.java
+++ src/main/java/com/kgstrivers/WorkT/Models/Author.java
@@ -1,6 +1,7 @@
 package com.kgstrivers.WorkT.Models;
 
 import javax.persistence.*;
+import java.util.Objects;
 import java.util.Set;
 
 
@@ -33,5 +34,28 @@ public class Author {
         this.firstname = firstname;
         this.lastname = lastname;
         this.books = books;
+    }
+
+    @Override
+    public String toString() {
+        return "Author{" +
+                "id=" + id +
+                ", firstname='" + firstname + '\'' +
+                ", lastname='" + lastname + '\'' +
+                ", books=" + books +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Author author = (Author) o;
+        return Objects.equals(id, author.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
     }
 }
